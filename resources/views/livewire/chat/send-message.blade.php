f898abd3c6abd6b0bfc19b9c82f093c5c7725d2f
rakhasf <rakhasaptafabian@gmail.com>
2023-06-02 10:37:45 +0700

    zzzzastava!!! commit

--- resources/views/livewire/chat/send-message.blade.php
+++ resources/views/livewire/chat/send-message.blade.php
@@ -1,6 +1,9 @@
 <div>
     {{-- The best athlete wants his opponent at his best. --}}
     <div class="chatbox-footer">
-        
+        <div class="custom_form_group">
+            <input type="text" class="control" placeholder="write message">
+            <button class="submit" type="submit">Send</button>
+        </div>
     </div>
 </div>
