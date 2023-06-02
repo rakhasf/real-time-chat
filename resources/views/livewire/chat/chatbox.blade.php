b34f245c6f0e54c0b378602979bf754ff5cfb5df
rakhasf <rakhasaptafabian@gmail.com>
2023-06-02 10:31:28 +0700

    chat design....... sigh

--- resources/views/livewire/chat/chatbox.blade.php
+++ resources/views/livewire/chat/chatbox.blade.php
@@ -1,10 +1,53 @@
 <div>
     {{-- The Master doesn't talk, he acts. --}}
     <div class="chatbox-header">
-        box
+        <div class="chatbox-header">
+            <div class="return">
+                <i class="bi bi-arrow-left"></i>
+            </div>
+            <div class="img-container">
+                <img src="https://picsum.photos/id/237/200/300" alt="">
+            </div>
+            <div class="name">
+                Alt
+            </div>
+            <div class="info">
+                <div class="info-item">
+                    <i class="bi bi-telephone-fill"></i>
+                </div>
+                <div class="info-item">
+                    <i class="bi bi-image"></i>
+                </div>
+                <div class="info-item">
+                    <i class="bi bi-info-circle"></i>
+                </div>
+            </div>
+        </div>
     </div>
 
     <div class="chatbox_body">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos eligendi error nemo rem aspernatur ipsum. Rem tempore placeat quae aperiam numquam praesentium esse hic, modi consectetur eaque in illo ut?
+        <div class="msg_body">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nulla quod corrupti facere reiciendis ut excepturi voluptas error nesciunt nostrum sed omnis incidunt minus, modi eaque sequi officia quos? Hic!
+        </div>
+        <div class="msg_body_footer">
+            <div class="date">
+                1 week ago
+            </div>
+            <div class="read">
+                <i class="bi bi-check-circle"></i>
+            </div>
+        </div>
+
+        <div class="msg_body msg-body-me">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nulla quod corrupti facere reiciendis ut excepturi voluptas error nesciunt nostrum sed omnis incidunt minus, modi eaque sequi officia quos? Hic!
+        </div>
+        <div class="msg_body_footer">
+            <div class="date">
+                1 week ago
+            </div>
+            <div class="read">
+                <i class="bi bi-check-circle"></i>
+            </div>
+        </div>
     </div>
 </div>
