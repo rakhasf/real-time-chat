78214d6abf93baebcb1063290b9d135e2aa1a29d
rakhasf <rakhasaptafabian@gmail.com>
2023-06-05 11:21:21 +0700

    mades tuff

--- resources/views/livewire/chat/create-chat.blade.php
+++ resources/views/livewire/chat/create-chat.blade.php
@@ -2,7 +2,7 @@
     {{-- Be like water. --}}
     <ul class="list-group w-75 mx-auto mt-3 container-fluid">
         @foreach ($users as $user)
-        <li class="list-group-item list-group-item-action">{{ $user->name }}</li>
+        <li class="list-group-item list-group-item-action" wire:click='checkConversation({{ $user->id }})'>{{ $user->name }}</li>
         @endforeach
     </ul>
 </div>
